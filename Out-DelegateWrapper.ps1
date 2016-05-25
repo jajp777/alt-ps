4e2fff5d87968d820037bdb8c2d3eea8df61234f
gregzakh <grishanz@yandex.ru>
2016-05-25 15:26:06 +0600

    Create Out-DelegateWrapper.ps1

---
+++ Out-DelegateWrapper.ps1
@@ -0,0 +1,99 @@
+<#
+  .NOTES
+      It creates a template for a script which uses delegates Func and Action.
+      See https://github.com/gregzakh/Func for more details.
+#>
+param(
+  [Parameter()]
+  [String]$Path = $PWD
+)
+
+$code = @'
+function Verb-Noun {
+  param()
+  
+  begin {
+    @(
+      [Runtime.InteropServices.CallingConvention],
+      [Runtime.InteropServices.HandleRef],
+      [Reflection.Emit.OpCodes]
+    ) | ForEach-Object {
+      $keys = ($ta = [PSObject].Assembly.GetType(
+        'System.Management.Automation.TypeAccelerators'
+      ))::Get.Keys
+    }{
+      if ($keys -notcontains $_.Name) {
+        $ta::Add($_.Name, $_)
+      }
+    }
+    
+    function private:Set-Delegate {
+      param(
+        [Parameter(Mandatory=$true, Position=0)]
+        [ValidateNotNullOrEmpty()]
+        [String]$Module,
+        
+        [Parameter(Mandatory=$true, Position=1)]
+        [ValidateNotNullOrEmpty()]
+        [String]$Function,
+        
+        [Parameter(Mandatory=$true, Position=2)]
+        [ValidateNotNullOrEmpty()]
+        [String]$Delegate
+      )
+      
+      begin {
+        [Regex].Assembly.GetType(
+          'Microsoft.Win32.UnsafeNativeMethods'
+        ).GetMethods() | Where-Object {
+          $_.Name -cmatch '\AGet(ProcA|ModuleH)'
+        } | ForEach-Object {
+          Set-Variable $_.Name $_
+        }
+        
+        $ptr = $GetProcAddress.Invoke($null, @(
+          [HandleRef](New-Object HandleRef(
+            (New-Object IntPtr), $GetModuleHandle.Invoke($null, @($Module))
+          )), $Function
+        ))
+      }
+      process { $proto = Invoke-Expression $Delegate }
+      end {
+        $method = $proto.GetMethod('Invoke')
+        
+        $returntype = $method.ReturnType
+        $paramtypes = $method.GetParameters() |
+                                        Select-Object -ExpandProperty ParameterType
+        
+        $holder = New-Object Reflection.Emit.DynamicMethod(
+          'Invoke', $returntype, $paramtypes, $proto
+        )
+        $il = $holder.GetILGenerator()
+        0..($paramtypes.Length - 1) | ForEach-Object {
+          $il.Emit([OpCodes]::Ldarg, $_)
+        }
+        
+        switch ([IntPtr]::Size) {
+          4 { $il.Emit([OpCodes]::Ldc_I4, $ptr.ToInt32()) }
+          8 { $il.Emit([OpCodes]::Ldc_I8, $ptr.ToInt64()) }
+        }
+        $il.EmitCalli(
+          [OpCodes]::Calli, [CallingConvention]::StdCall, $returntype, $paramtypes
+        )
+        $il.Emit([OpCodes]::Ret)
+        
+        $holder.CreateDelegate($proto)
+      }
+    }
+  }
+  process {
+  }
+  end {
+    [void]$ta::Remove('OpCodes')
+    [void]$ta::Remove('HandleRef')
+    [void]$ta::Remove('CallingConvention')
+  }
+}
+'@
+
+Out-File "$Path\Verb-Noun.ps1" -InputObject $code -Encoding Default -Force
