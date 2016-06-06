cd3b2a09428098b9dd4859eb0771f3d83bbe769b
gregzakh <grishanz@yandex.ru>
2016-06-06 23:58:36 +0500

    Update Get-FileCrc32.ps1

--- Get-FileCrc32.ps1
+++ Get-FileCrc32.ps1
@@ -107,8 +107,8 @@ function Get-FileCrc32 {
     }
   }
   end {
-    [void]$ta::Remove('OpCodes')
-    [void]$ta::Remove('HandleRef')
-    [void]$ta::Remove('CallingConvention')
+    'OpCodes', 'HandleRef', 'CallingConvention' | ForEach-Object {
+      [void]$ta::Remove($_)
+    }
   }
 }
