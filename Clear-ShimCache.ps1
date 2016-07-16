e54d0814a6b916dfad8214a06693f114779ec120
gregzakh <grishanz@yandex.ru>
2016-07-17 01:30:56 +0600

    Create Clear-ShimCache.ps1

---
+++ Clear-ShimCache.ps1
@@ -0,0 +1,18 @@
+function Clear-ShimCache {
+  <#
+    .SYNOPSIS
+        Flushes the application compatibility cache.
+    .DESCRIPTION
+        The data of cache is stored in the registry by path
+        HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\AppCompatibility
+  #>
+  
+  if ((New-Object Security.Principal.WindowsPrincipal(
+    [Security.Principal.WindowsIdentity]::GetCurrent()
+  )).IsInRole(
+    [Security.Principal.WindowsBuiltInRole]::Administrator
+  )) {
+    rundll32 kernel32.dll,BaseFlushAppcompatCache
+  }
+  else { Write-Warning "You should be an administrator." }
+}
