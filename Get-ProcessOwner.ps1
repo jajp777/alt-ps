60fea5d52f5bf84cd1e9a804cff55106707aaf5c
Grigori Zakharov <grishanz@yandex.ru>
2016-10-04 13:43:29 +0500

    Create Get-ProcessOwner.ps1

---
+++ Get-ProcessOwner.ps1
@@ -0,0 +1,42 @@
+#requires -version 5
+function Get-ProcessOwner {
+  <#
+    .SYNOPSIS
+        Retrieves owner of the specified process..
+  #>
+  param(
+    [Parameter(Mandatory=$true)]
+    [ValidateScript({($script:proc = Get-Process -Id $_) -ne 0})]
+    [Int32]$Id
+  )
+  
+  begin {
+    [Microsoft.Win32.SafeHandles.SafeAccessTokenHandle]$stah = [IntPtr]::Zero
+  }
+  process {
+    if (![Object].Assembly.GetType(
+      'Microsoft.Win32.Win32Native'
+    ).GetMethod(
+      'OpenProcessToken', [Reflection.BindingFlags]40
+    ).Invoke($null, ($par = [Object[]]@(
+      $proc.Handle, [Security.Principal.TokenAccessLevels]::Query, $stah
+    )))) {
+      $stah.Dispose()
+      throw New-Object ComponentModel.Win32Exception(
+        [Runtime.InteropServices.Marshal]::GetLastWin32Error()
+      )
+    }
+  }
+  end {
+    New-Object PSObject -Property @{
+      Process = $proc.Name
+      PID = $proc.Id
+      User = (New-Object Security.Principal.WindowsIdentity(
+        $par[2].DangerousGetHandle()
+      )).Name
+    } | Select-Object Process, PID, User | Format-List
+    
+    $par[2].Dispose()
+    $stah.Dispose()
+  }
+}
