f9c7bbefe73f694b4d5e78f30875220aaad94d5e
gregzakh <grishanz@yandex.ru>
2017-01-10 18:31:42 +0600

    Update Stop-CursorBlinking.ps1

--- misc/Stop-CursorBlinking.ps1
+++ misc/Stop-CursorBlinking.ps1
@@ -2,50 +2,50 @@ function Stop-CursorBlinking {
   <#
     .SYNOPSIS
         [Dis|En]ables blinking of the console cursor.
+    .NOTES
+        Author: greg zakharov
   #>
-  param([Switch]$Start)
-  
+  param([Switch]$Cancel)
+
   begin {
-    [Object].Assembly.GetType(
+    ($asm = [Object].Assembly.GetType(
       'Microsoft.Win32.Win32Native'
-    ).GetMethods([Reflection.BindingFlags]40) |
+    )).GetMethods([Reflection.BindingFlags]40) |
     Where-Object {
       $_.Name -cmatch '\A(Get|Set)(Std|ConsoleCursorI)'
     } | ForEach-Object {
       Set-Variable $_.Name $_
     }
-    
+
+    $STD_OUTPUT_HANDLE = $asm.GetField(
+      'STD_OUTPUT_HANDLE', [Reflection.BindingFlags]40
+    ).GetValue($null)
+
     $cci = [Activator]::CreateInstance(
       [Object].Assembly.GetType(
         'Microsoft.Win32.Win32Native+CONSOLE_CURSOR_INFO'
       )
     )
-    
-    $STD_OUTPUT_HANDLE = [Object].Assembly.GetType(
-      'Microsoft.Win32.Win32Native'
-    ).GetField(
-      'STD_OUTPUT_HANDLE', [Reflection.BindingFlags]40
-    ).GetValue($null)
   }
   process {
     $out = $GetStdHandle.Invoke($null, @($STD_OUTPUT_HANDLE))
-    
+
     if (!$GetConsoleCursorInfo.Invoke(
-      $null, ($par = [Object[]]@($out, $cci)))
-    ) {
+      $null, ($par = [Object[]]@($out, $cci))
+    )) {
       $err = [Runtime.InteropServices.Marshal]::GetLastWin32Error()
       throw (New-Object ComponentModel.Win32Exception($err)).Message
     }
-    
+
     if (($sz = $par[1].GetType().GetField(
       'dwSize', [Reflection.BindingFlags]36
-    ).GetValue($par[1])) -lt 1 -or $sz -gt 100) {
+    ).GetValue($par[1])) -lt 1 -and $sz -gt 100) {
       throw New-Object InvalidOperationException(
         'Cursor data has invalid size parameter.'
       )
     }
-    
-    $set = if ($Start) { $true } else { $false }
+
+    $set = if ($Cancel) { $true } else { $false }
     $par[1].GetType().GetField(
       'bVisible', [Reflection.BindingFlags]36
     ).SetValue($par[1], $set)
