bc321c0b9687755806c85501bf1371e1e2122d76
gregzakh <grishanz@yandex.ru>
2017-01-20 23:09:48 +0600

    Update Get-CpuFrequency.ps1

--- Get-CpuFrequency.ps1
+++ Get-CpuFrequency.ps1
@@ -1,103 +1,173 @@
-function Get-CpuFrequency {
+function Import-FromDll {
   <#
     .SYNOPSIS
-        Retrieves CPUs frequiensies.
+        Allows to invoke some WinAPI functions without using Add-Type
+        cmdlet.
+    .DESCRIPTION
+        This is possible due to reflection and Func and Action delegates.
+        Import-FromDll uses GetModuleHandle and GetProcAddress functions
+        stored into Microsoft.Win32.Win32Native type of mscorlib.dll
+    .PARAMETER Module
+        Library module name (DLL). Note that it should be currently loaded
+        by host, to check this:
+        
+       (ps -Is $PID).Modules | ? {$_.FileName -notmatch '(\.net|assembly)'}
+    .PARAMETER Signature
+        Signatures storage which presented as a hashtable.
+    .EXAMPLE
+        $kernel32 = Import-FromDll kernel32 -Signature @{
+          CreateHardLinkW = [Func[[Byte[]], [Byte[]], IntPtr, Boolean]]
+          GetCurrentProcessId = [Func[Int32]]
+        }
+        
+        $kernel32.GetCurrentProcessId.Invoke()
+        This will return value which is equaled $PID variable.
+        
+        $kernel32.CreateHardLinkW.Invoke(
+          [Text.Encoding]::Unicode.GetBytes('E:\to\target.ext'),
+          [Text.Encoding]::Unicode.GetBytes('E:\from\source.ext'),
+          [IntPtr]::Zero
+        )
+        Establishes a hard link between an existing file and a new file.
+    .OUTPUTS
+        Hashtable
     .NOTES
-        typedef _SYSTEM_BASIC_INFORMATION {
-            BYTE Reserved[43];
-            BYTE NumberOfProcessors; // +0x28
-        } SYSTEM_BASIC_INFORMATION, *PSYSTEM_BASIC_INFORMATION;
-        
-        typedef _PROCESSOR_POWER_INFORMATION {
-            ULONG Number;            // +0x00
-            ULONG MaxMhz;            // +0x04
-            ULONG CurrentMhz;        // +0x08
-            ULONG MhzLimit;          // +0x0c
-            ULONG MaxIdleState;      // +0x10
-            ULONG CurrentIdleState;  // +0x14
-        } PROCESSOR_POWER_INFORMATION, *PPROCESSOR_POWER_INFORMATION;
-        
-        SystemBasicInformation = 0
-        ProcessorInformation = 11
+        Author: greg zakharov
   #>
+  param(
+    [Parameter(Mandatory=$true, Position=0)]
+    [ValidateNotNullOrEmpty()]
+    [String]$Module,
+    
+    [Parameter(Mandatory=$true, Position=1)]
+    [ValidateNotNull()]
+    [Hashtable]$Signature
+  )
+  
   begin {
-    function private:New-Delegate {
+    function script:Get-ProcAddress {
+      [OutputType([Hashtable])]
       param(
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory=$true, Position=0)]
         [ValidateNotNullOrEmpty()]
         [String]$Module,
         
-        [Parameter(Mandatory=$true)]
-        [ValidateNotNullOrEmpty()]
-        [String]$Function,
-        
-        [Parameter(Mandatory=$true)]
-        [ValidateNotNullOrEmpty()]
-        [String]$Delegate
+        [Parameter(Mandatory=$true, Position=1)]
+        [ValidateNotNull()]
+        [String[]]$Function
       )
       
       begin {
         [Object].Assembly.GetType(
           'Microsoft.Win32.Win32Native'
-        ).GetMethods([Reflection.BindingFlags]40) |
-        Where-Object {
+        ).GetMethods(
+          [Reflection.BindingFlags]40
+        ) | Where-Object {
           $_.Name -cmatch '\AGet(ProcA|ModuleH)'
         } | ForEach-Object {
           Set-Variable $_.Name $_
         }
         
-        if (($ptr = $GetProcAddress.Invoke($null, @(
-          $GetModuleHandle.Invoke($null, @($Module)), $Function
-        ))) -eq [IntPtr]::Zero) {
+        if (($mod = $GetModuleHandle.Invoke(
+          $null, @($Module)
+        )) -eq [IntPtr]::Zero) {
           throw New-Object InvalidOperationException(
-            'Could not find specified signature.'
+            'Could not find specified module.'
           )
         }
       }
-      process { $proto = Invoke-Expression $Delegate }
+      process {}
       end {
-        $method = $proto.GetMethod('Invoke')
-        
-        $returntype = $method.ReturnType
-        $paramtypes = $method.GetParameters() |
-                    Select-Object -ExpandProperty ParameterType
-        
-        $holder = New-Object Reflection.Emit.DynamicMethod(
-          'Invoke', $returntype, $paramtypes, $proto
-        )
-        $il = $holder.GetILGenerator()
-        0..($paramtypes.Length - 1) | ForEach-Object {
-          $il.Emit([Reflection.Emit.OpCodes]::Ldarg, $_)
+        $table = @{}
+        foreach ($f in $Function) {
+          if (($$ = $GetProcAddress.Invoke(
+            $null, @($mod, $f)
+          )) -ne [IntPtr]::Zero) {
+            $table.$f = $$
+          }
         }
-        
-        switch ([IntPtr]::Size) {
-          4 { $il.Emit([Reflection.Emit.OpCodes]::Ldc_I4, $ptr.ToInt32()) }
-          8 { $il.Emit([Reflection.Emit.OpCodes]::Ldc_I8, $ptr.ToInt64()) }
-        }
-        $il.EmitCalli(
-          [Reflection.Emit.OpCodes]::Calli,
-          [Runtime.InteropServices.CallingConvention]::StdCall,
-          $returntype, $paramtypes
-        )
-        $il.Emit([Reflection.Emit.OpCodes]::Ret)
-        
-        $holder.CreateDelegate($proto)
+        $table
       }
     }
     
-    $NtQuerySystemInformation = New-Delegate ntdll NtQuerySystemInformation `
-                              '[Func[Int32, IntPtr, Int32, [Byte[]], Int32]]'
-    $NtPowerInformation = New-Delegate ntdll NtPowerInformation `
-                       '[Func[Int32, IntPtr, Int32, [Byte[]], Int32, Int32]]'
+    function private:New-Delegate {
+      param(
+        [Parameter(Mandatory=$true, Position=0)]
+        [ValidateScript({$_ -ne [IntPtr]::Zero})]
+        [IntPtr]$ProcAddress,
+        
+        [Parameter(Mandatory=$true, Position=1)]
+        [ValidateNotNull()]
+        [Type]$Prototype,
+        
+        [Parameter(Position=2)]
+        [ValidateNotNullOrEmpty()]
+        [Runtime.InteropServices.CallingConvention]
+        $CallingConvention = 'StdCall'
+      )
+      
+      $method = $Prototype.GetMethod('Invoke')
+      
+      $returntype = $method.ReturnType
+      $paramtypes = $method.GetParameters() |
+                          Select-Object -ExpandProperty ParameterType
+      
+      $holder = New-Object Reflection.Emit.DynamicMethod(
+        'Invoke', $returntype, $(
+          if (!$paramtypes) { $null } else { $paramtypes }
+        ), $Prototype
+      )
+      $il = $holder.GetILGenerator()
+      if ($paramtypes) {
+        0..($paramtypes.Length - 1) | ForEach-Object {
+          $il.Emit([Reflection.Emit.OpCodes]::Ldarg, $_)
+        }
+      }
+      
+      switch ([IntPtr]::Size) {
+        4 { $il.Emit([Reflection.Emit.OpCodes]::Ldc_I4, $ProcAddress.ToInt32()) }
+        8 { $il.Emit([Reflection.Emit.OpCodes]::Ldc_I8, $ProcAddress.ToInt64()) }
+      }
+      $il.EmitCalli(
+        [Reflection.Emit.OpCodes]::Calli, $CallingConvention, $returntype,
+        $(if (!$paramtypes) { $null } else { $paramtypes })
+      )
+      $il.Emit([Reflection.Emit.OpCodes]::Ret)
+      
+      $holder.CreateDelegate($Prototype)
+    }
+  }
+  process {}
+  end {
+    $scope, $fname = @{}, (Get-ProcAddress $Module $Signature.Keys)
+    foreach ($key in $fname.Keys) {
+      $scope[$key] = New-Delegate $fname[$key] $Signature[$key]
+    }
+    $scope
+  }
+}
+
+function Get-CpuFrequency {
+  <#
+    .SYNOPSIS
+        Retrieves CPUs frequensies.
+    .NOTES
+        Author: greg zakharov
+  #>
+  begin {
+    $ntdll = Import-FromDll ntdll -Signature @{
+      NtPowerInformation = [Func[Int32, IntPtr, Int32, [Byte[]], Int32, Int32]]
+      NtQuerySystemInformation = [Func[Int32, IntPtr, Int32, [Byte[]], Int32]]
+    }
   }
   process {
     try {
       # sizeof(SYSTEM_BASIC_INFORMATION) = 44
       $sbi = [Runtime.InteropServices.Marshal]::AllocHGlobal(44)
-      
-      if ($NtQuerySystemInformation.Invoke(0, $sbi, 44, $null) -ne 0) {
+
+      if ($ntdll.NtQuerySystemInformation.Invoke(0, $sbi, 44, $null) -ne 0) {
         throw New-Object InvalidOperationException(
-          'Could not retrieve number of processors.'
+          'Could not retrive number of processors.'
         )
       }
       # NumberOfProcessors
@@ -105,35 +175,38 @@ function Get-CpuFrequency {
       # sizeof(PROCESSOR_POWER_INFORMATION) = 24
       $len = 24 * $nop # correct buffer size
       $buf = New-Object Byte[]($len)
-      if ($NtPowerInformation.Invoke(11, [IntPtr]::Zero, 0, $buf, $len) -ne 0) {
+      if ($ntdll.NtPowerInformation.Invoke(11, [IntPtr]::Zero, 0, $buf, $len) -ne 0) {
+        Remove-Variable buf
         throw New-Object InvalidOperationException(
-          'Could not retrieve processor power information.'
+          'Could not retrieve processor(s) power information.'
         )
       }
-      # getting PROCESSOR_POWER_INFORMATION for each processor
-      $j = 0
-      for ($i = 0; $i -lt $nop; $i++) {
-        [Byte[]]$tmp = $buf[$j..($j + 23)]
-        $gch = [Runtime.InteropServices.GCHandle]::Alloc($tmp, 'Pinned')
-        $ptr = $gch.AddrOfPinnedObject()
-        New-Object PSObject -Property @{
-          Number = [Runtime.InteropServices.Marshal]::ReadInt32($ptr)
-          MaxMhz = [Runtime.InteropServices.Marshal]::ReadInt32($ptr, 0x04)
-          CurrentMhz = [Runtime.InteropServices.Marshal]::ReadInt32($ptr, 0x08)
-          MhzLimit = [Runtime.InteropServices.Marshal]::ReadInt32($ptr, 0x0c)
-          MaxIdleState = [Runtime.InteropServices.Marshal]::ReadInt32($ptr, 0x10)
-          CurrentIdleState = [Runtime.InteropServices.Marshal]::ReadInt32($ptr, 0x14)
-        } |
-        Select-Object Number, MaxMhz, CurrentMhz, MhzLimit, MaxIdleState, CurrentIdleState
-        $gch.Free()
-        
-        $j += 24
-      }
     }
-    catch { $_.Exception }
+    catch { Write-Verbose $_ }
     finally {
       if ($sbi) { [Runtime.InteropServices.Marshal]::FreeHGlobal($sbi) }
     }
   }
-  end {}
+  end {
+    if (!$buf) { return }
+    # getting PROCESSOR_POWER_INFORMATION for each processor
+    $j = 0
+    for ($i = 0; $i -lt $nop; $i++) {
+      [Byte[]]$tmp = $buf[$j..($j + 23)]
+      $gch = [Runtime.InteropServices.GCHandle]::Alloc($tmp, 'Pinned')
+      $ptr = $gch.AddrOfPinnedObject()
+      New-Object PSObject -Property @{
+        Number           = [Runtime.InteropServices.Marshal]::ReadInt32($ptr)
+        MaxMhz           = [Runtime.InteropServices.Marshal]::ReadInt32($ptr, 0x04)
+        CurrentMhz       = [Runtime.InteropServices.Marshal]::ReadInt32($ptr, 0x08)
+        MhzLimit         = [Runtime.InteropServices.Marshal]::ReadInt32($ptr, 0x0c)
+        MaxIdleState     = [Runtime.InteropServices.Marshal]::ReadInt32($ptr, 0x10)
+        CurrentIdleState = [Runtime.InteropServices.Marshal]::ReadInt32($ptr, 0x14)
+      } |
+      Select-Object Number, MaxMhz, CurrentMhz, MhzLimit, MaxIdleState, CurrentIdleState
+      $gch.Free()
+
+      $j += 24
+    }
+  }
 }
